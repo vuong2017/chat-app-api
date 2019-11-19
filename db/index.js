63d5ae83d055b8df80afde6617bad4b9272cc1d5
Vuong 2017 <phanquyenvuong@gmail.com>
2019-11-20 01:07:48 +0700

    sd

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = () => {
   return new Promise(success => {
-    mongoose.connect(process.env.DBURL, { useNewUrlParser: true });
+    mongoose.connect("mongodb://admin:A123456@ds141198.mlab.com:41198/daulo", { useNewUrlParser: true });
     mongoose.connection.on(
       "error",
       console.error.bind(console, "connection error:")
