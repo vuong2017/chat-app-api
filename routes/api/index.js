683f6a7329921bf2b5dcf7df301ad3076a4e0839
Vuong 2017 <phanquyenvuong@gmail.com>
2019-11-20 01:12:12 +0700

    sd

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 const router = express.Router();
 
-import user from "./user";
+
+
+import user from "../../controllers/user";
+import { authMiddleware } from "../../middlewares/auth";
+
+// import user from "./user";
 import twoFactor from "./two-factor";
 import chatRoom from "./chat-room";
 import chatMessage from "./chat-message";
 
 
-router.get("/aa", (req,res) => res.json({a:1}))
+router.get("/aa", (req,res) => res.json({a:process.env.DBURL}))
+router.get("/bb", authMiddleware, user.get)
 
-router.use("/user", user);
+
+// router.use("/user", user);
 router.use("/two-factor", twoFactor);
 router.use("/chat-room", chatRoom);
 router.use("/chat-message", chatMessage);
